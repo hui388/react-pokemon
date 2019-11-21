7a271ed8090f570334706de0907639a065439f26
Mary <maryliu.388@gmail.com>
2019-11-21 08:12:24 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './mysass.scss'
+import Select from 'react-select'
+import Search from "./components/Search"
+
+const types = [
+  { value: 'normal', label: 'Normal' },
+  { value: 'grass', label: 'Grass' },
+  { value: 'fire', label: 'Fire' },
+];
+
+const colors = [
+  { value: 'pink', label: 'Pink' },
+  { value: 'green', label: 'Green' },
+  { value: 'red', label: 'Red' },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <h1 className="App-title">Pokémon Team Generator</h1>
+        </header>
+        <div className="form-container">
+            <h2 className="category">Type</h2>
+            <Select className="selector" value="chocolate" options={types} />
+            <h2 className="category">Second Type</h2>
+            <Select className="selector" value="chocolate" options={types} />
+            <h2 className="category">Color</h2>
+            <Select className="selector" value="chocolate" options={colors} />
+            <button className="form-team">Form Team!</button>
+        </div>
+
     </div>
   );
 }
