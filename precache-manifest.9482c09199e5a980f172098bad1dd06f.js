682e3670173f561e2a591b07b28fef042813bfd1
Mary <maryliu.388@gmail.com>
2020-02-01 17:23:12 -0500

    Updates

---
+++ precache-manifest.9482c09199e5a980f172098bad1dd06f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b771e86674fc03fe8941a913b16829e",
+    "url": "/react-pokemon/index.html"
+  },
+  {
+    "revision": "f1ff5d71f195e04b559b",
+    "url": "/react-pokemon/static/css/main.12dbc30a.chunk.css"
+  },
+  {
+    "revision": "6d00e20f566bf0420f6f",
+    "url": "/react-pokemon/static/js/2.8c76b6f4.chunk.js"
+  },
+  {
+    "revision": "f1ff5d71f195e04b559b",
+    "url": "/react-pokemon/static/js/main.fe7cc45b.chunk.js"
+  },
+  {
+    "revision": "a483039d5a1c3eebd6be",
+    "url": "/react-pokemon/static/js/runtime-main.8b47bafa.js"
+  },
+  {
+    "revision": "c4d7245371dd1c906aad5bc63894ca99",
+    "url": "/react-pokemon/static/media/pokeball.c4d72453.png"
+  }
+]);
